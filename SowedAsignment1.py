c5298b6b2bbadbcdbf32ec4044005ea3b6531513
Imrasoft <mutumba.sowed@gmail.com>
2017-01-06 18:06:42 +0300

    Update SowedAsignment1.py

--- SowedAsignment1.py
+++ SowedAsignment1.py
@@ -1,4 +1,7 @@
+#Import the Counter Function from the Collections Module
 from collections import Counter
+#Ask the user to Enter an SMS
+#The SMS is Entered as a string so that it can be split with the Split Function
 sms = input ("Please Enter an SMS")
 word = sms.split()
 print (word)
